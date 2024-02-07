999dd10ef9933bec137caade7b36c9c147600d8c
Zinnia <120154112+zinnia-frazier@users.noreply.github.com>
2024-02-06 17:23:25 -0800

    Bedtime functions outlined

--- calculator.js
+++ calculator.js
@@ -1,3 +1,19 @@
 const sleepLength = {
     sleepCycleHours: 1.5
 };
+
+const whenGoToSleep = (desiredWakeUpTime, numberOfSleepCycles) => {
+    return sleepTime = desiredWakeUpTime - numberOfSleepCycles;
+}
+
+const whenGoToSleepDisplay = () => {
+    document.getElementById('bedtime').innerHTML = whenGoToSleep();
+}
+
+const alarmTimeCalculator = () => {
+
+}
+
+const sleepDebtCalculator = () => {
+
+}
