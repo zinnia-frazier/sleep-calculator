794eba0ce680e7ec1960ce5332025013e5d6675d
Zinnia <120154112+zinnia-frazier@users.noreply.github.com>
2024-03-24 17:05:30 -0700

    Bedtime component functionality successful

--- sleep-calculator/src/components/Bedtime.js
+++ sleep-calculator/src/components/Bedtime.js
@@ -5,42 +5,40 @@ import React, { useState } from 'react';
 // Export component
 export default function Bedtime() {
 
-    const [bedtime, setBedtime] = useState('');
     const [wakeTime, setWakeTime] = useState('');
-    const [clockSign, setClockSign] = useState(' P.M.');
-    const [userNumber, setUserNumber] = useState('');
 
     // Handle Change
     function handleChange(event) {
+
+        // Get form select as number
         setWakeTime(event.target.value);
     }
-    
-    // Handle Submit
-    function handleSubmit(event) {
-        event.preventDefault();
-        setWakeTime(parseFloat(event.target.value));
+
+    // Convert form select to number if not a number
+    if (wakeTime === isNaN) {
+        setWakeTime(parseFloat(wakeTime));
     }
-        
-    // Sleep Calculator
-    function sleepCalculator(wakeTime) {        
 
-        if (wakeTime < 1) {
-            setUserNumber(wakeTime + 12);
-            setClockSign(' P.M.');
-        } else {
-            setClockSign(' A.M.');
-        }
+    let clockSign = ' A.M.';
 
-        if (wakeTime === 12) {
-            setClockSign(' A.M.');
-        }
+    // Set bedtime to be the waketime minus 8 hours
+    let bedtime = wakeTime - 8;
+    
+    // If the waketime is in the negatives, add 12 hours and change the clock sign to P.M.
+    if (bedtime <= 0) {
+        bedtime = bedtime + 12;
+        clockSign = ' P.M.';
+    } else if (wakeTime === 12) {
+        clockSign = ' A.M.';
+    } else {
+        clockSign = ' A.M.';
+    }
 
-        if (!isNaN(wakeTime)) {
-            setBedtime('Go to bed by ' + wakeTime - 8 + clockSign);
+    // Bedtime statement with the bedtime and clock sign
+    let bedtimeStatement = 'Go to bed by ' + bedtime + clockSign;
 
-        } else {
-            setBedtime('Please enter a valid number.')
-        }
+    if (bedtime === isNaN) {
+        bedtimeStatement = 'Please enter a valid number.';
     }
 
     return (
@@ -49,10 +47,10 @@ export default function Bedtime() {
 
             <h2>When Should I go to Sleep</h2>
 
-            <form onSubmit={handleSubmit}>
+            <form>
               <label for="sleepTimeSelect">Desired wake up time?</label>
 
-                <select id="sleepTimeSelect" name="bedTimecalculator">
+                <select onChange={handleChange} id="sleepTimeSelect" name="bedTimecalculator">
                     <option value="1">1 A.M.</option>
                     <option value="2">2 A.M.</option>
                     <option value="3">3 A.M.</option>
@@ -67,17 +65,15 @@ export default function Bedtime() {
                     <option value="12">12 P.M.</option>
                 </select>
 
-                <input type='number' value={wakeTime} onChange={handleChange} />
-
-                <button type='submit' onClick={sleepCalculator}>Calculate my Bed Time</button>
             </form>
             
 
-            <h3>{bedtime}</h3>
+            <h3>{bedtimeStatement}</h3>
 
-            <p>{wakeTime}</p>
+            <p>Wake Time {wakeTime}</p>
 
-            <p>{userNumber}</p>
+            <p>Bed Time {bedtime}</p>
+
         </main>
     )
 }
